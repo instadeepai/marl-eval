14f69dde0ab6a05c10ffa3e2e7951705526ba9cb
RuanJohn <ruanjdekock@gmail.com>
2022-09-22 18:26:17 +0200

    feat: added data processing utils test

---
+++ tests/data_processing_utils_test.py
@@ -0,0 +1,75 @@
+# python3
+# Copyright 2021 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for data processing utils"""
+
+import json
+from typing import Any, Dict, Mapping
+
+import jax
+import numpy as np
+import pytest
+from expected_test_data import (
+    matrix_1_expected_data,
+    sample_efficiency_matrix_expected_data,
+)
+
+from marl_tools.utils.data_processing_utils import (
+    create_matrices_for_rliable,
+    data_process_pipeline,
+)
+
+
+@pytest.fixture
+def raw_data() -> Mapping[str, Dict[str, Any]]:
+    """Fixture for raw experiment data."""
+    with open("tests/mock_data_test.json", "r") as f:
+        read_in_data = json.load(f)
+
+    return read_in_data
+
+
+def test_data_processing_pipeline(raw_data: Mapping[str, Dict[str, Any]]) -> None:
+    """Tests whether data processing pipeline runs."""
+
+    processed_data = data_process_pipeline(  # noqa
+        raw_data=raw_data, metrics_to_normalize=["return"]
+    )
+
+
+def test_matrices_for_rliable(raw_data: Mapping[str, Dict[str, Any]]) -> None:
+    """Tests that arrays for rliable are created correctly."""
+
+    processed_data = data_process_pipeline(
+        raw_data=raw_data, metrics_to_normalize=["return"]
+    )
+
+    m1, m2 = create_matrices_for_rliable(
+        data_dictionary=processed_data,
+        environment_name="SMAC",
+        metrics_to_normalize=["return"],
+    )
+
+    # Test that all arrays are equal.
+    jax.tree_util.tree_map(
+        lambda x, y: np.testing.assert_allclose(x, y, rtol=0.0, atol=1e-05),
+        m1,
+        matrix_1_expected_data,
+    )
+    jax.tree_util.tree_map(
+        lambda x, y: np.testing.assert_allclose(x, y, rtol=0.0, atol=1e-05),
+        m2,
+        sample_efficiency_matrix_expected_data,
+    )
