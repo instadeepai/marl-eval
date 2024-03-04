d90dee516cae80b3115d2f8757e2d834845a4ea6
Jemma Daniel <jemma.l.daniel@gmail.com>
2024-03-04 18:47:40 +0200

    chore: small dictionary change

--- marl_eval/utils/data_processing_utils.py
+++ marl_eval/utils/data_processing_utils.py
@@ -261,10 +261,10 @@ def data_process_pipeline(  # noqa: C901
                                             "global_max"
                                         ]
                                         # Use the custom min or max if given
-                                        if task in custom_min.keys():
-                                            metric_global_min = custom_min[task]
-                                        if task in custom_max.keys():
-                                            metric_global_max = custom_max[task]
+                                        if task in custom_min.keys() and metric in custom_min[task].keys():
+                                            metric_global_min = custom_min[task][metric]
+                                        if task in custom_max.keys() and metric in custom_max[task].keys():
+                                            metric_global_max = custom_max[task][metric]
                                         normed_metric_array = (
                                             metric_array - metric_global_min
                                         ) / (
